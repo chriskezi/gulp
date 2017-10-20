0b78c2a194c24c1fbe8b326a7a6c0162afa2d131
Krisztina Kezi <krisztinakezi@Krisztinas-MacBook-Pro.local>
2017-10-20 09:46:15 +0200

    intalled bower, bootsrap-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require ("gulp");
 var sass = require ("gulp-sass");
+var bower = require ("gulp-bower");
+var notify = require ("gulp-notify");
+
 
 gulp.task ('sass', function() {
   return gulp.src ('./sass/*.scss')
@@ -7,7 +10,30 @@ gulp.task ('sass', function() {
     .pipe(gulp.dest('./css'));
 
 });
+
+
 gulp.task ('watch', function(){
   gulp.watch('./sass/*.scss', ['sass']);
 });
 gulp.task('default', ['sass', 'watch']);
+
+
+// Bootsrap
+
+var bsConfig = require("gulp-bootstrap-configurator");
+
+// For CSS
+gulp.task('make-bootstrap-css', function(){
+  return gulp.src("./config.json")
+    .pipe(bsConfig.css())
+    .pipe(gulp.dest("./assets"));
+    // It will create `bootstrap.css` in directory `assets`.
+});
+
+// For JS
+gulp.task('make-bootstrap-js', function(){
+  return gulp.src("./config.json")
+    .pipe(bsConfig.js())
+    .pipe(gulp.dest("./assets"));
+    // It will create `bootstrap.js` in directory `assets`.
+});
